9e84be21dda9798bfdba5efc7a0eb38ff45415c9
walze <wallacefares@gmail.com>
2021-08-11 10:14:37 -0300

    added open api

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 import { initSql } from './db';
 
 async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
   initSql();
 
-  const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Parking Lot API')
+    .setDescription('Alocate Lots to incoming cars')
+    .setVersion('1.0')
+    .addTag('car')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 
